dc0c53fbe4960365ac2778fe845dc07983d1ff01
lizichen <phdinusa@gmail.com>
2017-02-25 21:36:21 -0500

    success wonder script

--- BashScript_AllPublications/wonder_script.sh
+++ BashScript_AllPublications/wonder_script.sh
@@ -3,8 +3,22 @@
 #echo "creating cookie file"
 #cat url_scopus | xargs curl -c cookies.txt -I
 
-echo "downloading csv file"
-cat url_scopus | xargs curl -b cookies.txt > author.csv
+#variables: scopusID, author.csv
+
+#echo "downloading csv file"
+#cat url_scopus | xargs curl -b cookies.txt > author.csv
+
+#nl url_scopus | xargs curl -b cookies.txt sh -c '"$1" > logfile-$0'
+#nl url_scopus | xargs -n 2 -P 8 sh -c './myScript.sh "$1" > logfile-$0'
+nl url_scopus | xargs -n 2 -P 8 sh -c 'curl -b cookies.txt "$1" > logfile-$0.csv'
 
 
-# cat url_scopus | xargs curl -X POST -F 'username=username' -F 'password=password' -b cookies.txt > author.csv
+
+#7006843462
+#56261433100
+#7005071296
+#7102605212
+#7102080550
+#6601983917
+#7102014925
+
