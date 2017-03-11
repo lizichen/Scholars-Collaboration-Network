598362a3f636e197e0573aa3fab80351ed2d61ef
lizichen <phdinusa@gmail.com>
2017-03-11 13:12:35 -0500

    process html ipynb

--- BashScript_AllPublications/all-authors-html/get_authors_html_page.sh
+++ BashScript_AllPublications/all-authors-html/get_authors_html_page.sh
@@ -4,9 +4,9 @@
 # if search for firstname = James, lastname = Kurose, we have a website specifically for all possible James Kurose
 #nl ../../authorslist_urls.txt | xargs -n 2 -P 8 sh -c 'curl -b cookies_feb28.txt "$1" > author-$0.html'
 
-let COUNTER=0
+let COUNTER=$2
 while [ $COUNTER -lt 10 ]; read NAME
      do curl -b ../cookies/cookies_feb28.txt "$NAME" > author_$COUNTER.html
      sleep 0.5
      let COUNTER=COUNTER+1
-done < authors_urls.txt
+done < $1
