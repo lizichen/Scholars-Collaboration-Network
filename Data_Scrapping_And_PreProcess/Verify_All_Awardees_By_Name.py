b02f947772df14fc390eb16ef803dd92a16e792e
lizichen <lizichen@gmail.com>
2017-03-16 11:12:04 -0700

    add 4202

--- Data_Scrapping_And_PreProcess/Verify_All_Awardees_By_Name.py
+++ Data_Scrapping_And_PreProcess/Verify_All_Awardees_By_Name.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[13]:
-
 # Read the Investigator.csv file for all the names.
 # Find accordingly the name in all-authors-csv
 import glob
@@ -11,16 +6,24 @@ import sys
 
 target_file = open('MISSING_AWARDEES.txt', 'w')
     
-count = 1
+mis_counter = 1
+csv_counter = 1
+write_counter = 0
+
 with open("investigator.csv", "rb") as f:
     reader = csv.reader(f, delimiter=",")
     for i, line in enumerate(reader):
         search = line[1] + "_" + line[2]
-        if len(glob.glob('all-authors-csv/'+search+'*.csv')) == 0:
-            print str(count) + " " + search
+        if len(glob.glob('authors_csv/'+search+'*.csv')) == 0:
+            if write_counter % 100 == 0:
+                target_file.close()
+                target_file = open('MISSING_AWARDEES.txt', "a+")
+            print "(" + str(mis_counter) + "/" + str(csv_counter) + ")_" + search
             target_file.write(search.encode('utf-8'))
             target_file.write("\n")
-            count += 1
+            mis_counter += 1
+            write_counter += 1
+        csv_counter += 1 # increment 1 by reading csv file line-by-line
 
 target_file.close()
 
